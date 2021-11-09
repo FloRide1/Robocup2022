ad4b339d22a9daad824c7bac84fa402d2f46e1c0
Valentin Gies <gies@univ-tln.fr>
2021-11-10 00:09:00 +0100

    ajout playbook

---
+++ C#/PlayBook/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace PlayBook_NS
+{
+    public class PlayBook
+    {
+        Dictionary<string, PlayCard> playingCards = new Dictionary<string, PlayCard>();
+    }
+
+    public class PlayCard
+    {
+        Dictionary<int, Location> preferredLocation = new Dictionary<int, Location>();
+        Dictionary<int, List<Comportment>> preferredComportements = new Dictionary<int, List<Comportment>>();
+        Dictionary<int, RectangleD> preferredZones = new Dictionary<int, RectangleD>();
+    }
+
+    public enum Comportment
+    {
+        Waiting,
+        Pressing,
+        Unmarking,
+        BetweenOwnGoalAndBall_2m,
+        BetweenOwnGoalAndBall_5m,
+        PrepareForPassToTeammate,
+    }
+
+    public enum PlayingSituations
+    {
+        Defense,
+        Attack,
+        Kickoff,
+        ThrowInFor,
+        ThrowInAgainst,
+        CornerFor,
+        CornerAgainst,
+        PenaltyFor,
+        PenaltyAgainst,
+        GoalKickFor,
+        GoalKickAgainst,
+
+    }
+
+
+    //public class Comportment
+    //{
+
+    //}
+}
